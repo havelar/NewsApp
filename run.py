bea7ee9c3187f9d93142239770722173ea94a9ff
havelar <amaral_henrique@offerwise.com>
2020-03-17 09:31:35 -0300

    Changing run.py

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from appLink import app
 
-HOST = '192.168.100.142'
+#HOST = '192.168.100.142'
 #HOST = '192.168.0.4'
 #HOST = '172.20.10.2'
+HOST = 0.0.0.0
 
 
-app.run(host=HOST, port='8000', debug=True)
+#app.run(host=HOST, port='8000', debug=True)
+app.run(debug=True)
