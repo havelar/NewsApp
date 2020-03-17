c89db1c429f9f297cb96202da590b5eba47c28d9
havelar <amaral_henrique@offerwise.com>
2020-03-17 09:45:09 -0300

    fixing

--- appLink/app.py
+++ appLink/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_cors import CORS
 
-from appLink.routes.linkRoute import linksBP
+from routes.linkRoute import linksBP
 
 app = Flask(__name__)
 CORS(
